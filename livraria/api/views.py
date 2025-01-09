f6c1231f9c8b8e126d159188646427b0b89a1f01
Kethelyn <kethcavalari@gmail.com>
2025-01-09 14:54:26 -0300

    adicionando JWT

--- livraria/api/views.py
+++ livraria/api/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Livro
 from .serializers import livroSerializers
 
 class LivroListCreateView(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = livroSerializers
+    permission_classes = [permissions.IsAuthenticated] #exige autenticação
 
 class LivroRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = livroSerializers
+    permission_classes = [permissions.IsAuthenticated]
